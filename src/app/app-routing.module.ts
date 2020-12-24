1e591ada82b4ddf2ecd0beed657146608c8bd631
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-12-24 14:43:43 +0700

    create new product component done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {CreateProductComponent} from './product/create-product/create-product.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'create-product',
+  component: CreateProductComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/product/create-product/create-product.component.ts
@@ -0,0 +1,26 @@
+import {Component, OnInit} from '@angular/core';
+import {Product} from '../../model/product';
+import {ProductService} from '../../service/product/product.service';
+
+@Component({
+  selector: 'app-create-product',
+  templateUrl: './create-product.component.html',
+  styleUrls: ['./create-product.component.css']
+})
+export class CreateProductComponent implements OnInit {
+  product: Product = {};
+
+  constructor(private productService: ProductService) {
+  }
+
+  ngOnInit() {
+  }
+
+  createProduct() {
+    this.productService.createNewProduct(this.product).subscribe(() => {
+      console.log('Thành công');
+    }, () => {
+      console.log('Xảy ra lỗi');
+    });
+  }
+}
